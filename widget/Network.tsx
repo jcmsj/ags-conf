b342bfedd249d185866a5e40727a507b9dbd4785
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2025-07-21 13:48:04 +0800

    fix(Network): refactor network panel to use bindings and improve tooltip functionality

--- widget/Network.tsx
+++ widget/Network.tsx
@@ -1,50 +1,50 @@
-import { Gtk } from "ags/gtk4";
+import { createBinding, createComputed } from "ags";
+import { exec } from "ags/process";
 import Network from "gi://AstalNetwork";
+import AstalNetwork from "gi://AstalNetwork?version=0.1";
 
 export function NetworkPanel() {
     const network = Network.get_default()!;
-    // for now, no actions, just show network status
-    // for the popup button, just show the wired/wireless and network strength via icons
-    
-    // for the popover content,
-    // show network name, type (wired/wireless), and strength
+    const wifi = createBinding(network, "wifi");
+    const wired = createBinding(network, "wired");
+    const activeConnectionId = createComputed([wifi], (wifi) => {
+        const it = wifi.enabled ? wifi.activeConnection.id : null;
+        return `Wi-Fi: ${it ? it.toString() : "Disabled"}`;
+    });
+    const signalStrength = createComputed([wifi], (wifi) => {
+        return `Signal Strength: ${wifi.enabled ? wifi.strength.toString()  + "%" : "N/A"}`;
+    });
+    const handleNetworkClick = () => {
+        exec("rofi-network-manager");
+    };
+
+    const getTooltipText = () => {
+        if (wifi.get().enabled) {
+            return `${activeConnectionId.get()}\n${signalStrength.get()}`;
+        } else if (wired.get().internet == AstalNetwork.Internet.CONNECTED) {
+            return `Wired: ${network.wired.connection.id}\nSpeed: ${network.wired.speed} Mbps`;
+        } else {
+            return "No Network Connection";
+        }
+    };
+
     return (
         <box class="Network Panel SingleItem">
-            <menubutton widthRequest={5} heightRequest={5} tooltipText="Network Options">
-                {/* popup button via image */}
+            <button 
+                widthRequest={5} 
+                heightRequest={5} 
+                tooltipText={getTooltipText()}
+                onClicked={handleNetworkClick}
+            >
                 {
-                    network.wifi.enabled ?
-                    <image iconName={network.wifi.iconName} tooltipText={network.wifi.activeConnection.id} /> :
-
-                    network.wired.enabled ?
-                    <image iconName={network.wired.iconName} tooltipText={network.wired.connection.id} />
+                    wifi.as(it => it.enabled) ?
+                    <image iconName={wifi.get().iconName} /> :
+                    wired.as(it => it.internet == AstalNetwork.Internet.CONNECTED)?
+                    <image iconName={wired.get().iconName} />
                     :
-                    <image iconName="network-disconnected-symbolic" tooltipText="No Network Connection" />
+                    <image iconName="network-disconnected-symbolic" />
                 }
-                
-                {/* <image iconName={ ? "network-wireless-connected-symbolic" : "network-wireless-disconnected-symbolic"} /> */}
-                <popover>
-                    <box orientation={Gtk.Orientation.VERTICAL} class={'text-left'}>
-                        {/* diff results based on wifi/wired/disabled */}
-                        {
-                            network.wifi.enabled ? (
-                                <>
-                                    <label label={`Wi-Fi: ${network.wifi.activeConnection.id}`} />
-                                    <label label={`Signal Strength: ${network.wifi.strength}%`} />
-                                </>
-                            ) : network.wired.enabled ? (
-                                <>
-                                    <label label={`Wired: ${network.wired.connection.id}`} />
-                                    <label label={`Speed: ${network.wired.speed} Mbps`} />
-                                    <image iconName={network.wired.iconName} />
-                                </>
-                            ) : (
-                                <label label="No Network Connection" />
-                            )
-                        }
-                        </box>
-                </popover>
-            </menubutton>
+            </button>
         </box>
     );
 }
